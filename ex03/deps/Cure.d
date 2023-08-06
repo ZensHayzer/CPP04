add22f1011a183df48c3b9ab4697f8a4d08d64c1
ajeanne <ajeanne@student.42.fr>
2023-08-06 03:34:46 +0200

    toto terminado

--- ex03/deps/Cure.d
+++
@@ -1,8 +0,0 @@
-objects/Cure.o: srcs/Cure.cpp includes/Cure.hpp includes/AMateria.hpp \
-  includes/ICharacter.hpp
-
-includes/Cure.hpp:
-
-includes/AMateria.hpp:
-
-includes/ICharacter.hpp:
--- ex03/deps/Ice.d
+++
@@ -1,8 +0,0 @@
-objects/Ice.o: srcs/Ice.cpp includes/Ice.hpp includes/AMateria.hpp \
-  includes/ICharacter.hpp
-
-includes/Ice.hpp:
-
-includes/AMateria.hpp:
-
-includes/ICharacter.hpp:
